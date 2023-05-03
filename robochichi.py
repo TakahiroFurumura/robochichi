52afb4f5ee0fc2bbabd8ab4485b411ed1563c2a7
TakahiroFurumura <55015324+TakahiroFurumura@users.noreply.github.com>
2023-05-03 21:00:52 +0900

    initial commit

--- robochichi.py
+++ robochichi.py
@@ -2,6 +2,10 @@ from flask import Flask
 
 app = Flask(__name__)
 
+@app.route("/")
+def hello_world():
+    return "<p>top</p>"
+
 @app.route("/chatapi/line")
 def hello_world():
     return "<p>line response</p>"
