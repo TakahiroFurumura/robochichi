273a20906be8a2a1b5f30c7af0dc0b662dc770b0
TakahiroFurumura <55015324+TakahiroFurumura@users.noreply.github.com>
2023-05-05 11:43:04 +0900

    initial commit

--- robochichi.py
+++ robochichi.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
@@ -6,9 +6,19 @@ app = Flask(__name__)
 def top():
     return "<p>top</p>"
 
+
+@app.route("/test")
+def test():
+    message: dict = {'message': 'this is test message'}
+    body = request.get_json()
+    parameter = request.args.to_dict()
+    return jsonify(parameter.update(body.update(message)))
+
+
 @app.route("/chatapi/line")
 def line():
     return "<p>line response</p>"
+
 
 if __name__ == '__main__':
     app.run()
