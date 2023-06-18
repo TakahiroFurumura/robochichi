2053969f086a76499018a61619cc915c4181fca0
TakahiroFurumura <55015324+TakahiroFurumura@users.noreply.github.com>
2023-06-18 16:16:29 +0900

    initial commit

--- robochichi.py
+++ robochichi.py
@@ -104,7 +104,7 @@ def line():
                         if response_message is not None:
                             line_bot_api.reply_message(
                                 event.get('replyToken'),
-                                TextSendMessage(text=response_message)
+                                TextSendMessage(text=response_message+response_message)
                             )
                     else:
                         pass
